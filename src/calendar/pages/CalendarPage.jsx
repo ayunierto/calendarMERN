4fbd8bf46ad303a0ca9e69f9330a5bde7c92aa5a
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-06-30 07:00:09 -0500

    react bog calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,48 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { format, parse, startOfWeek,getDay, addHours } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
+
 import { NavBar } from "../components/NavBar"
 
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
 export const CalendarPage = () => {
+
+    const locales = {
+        'en-US': enUS,
+      }
+
+      const localizer = dateFnsLocalizer({
+        format,
+        parse,
+        startOfWeek,
+        getDay,
+        locales,
+      })
+
+    const events = [{
+        title: 'Birthday',
+        notes: 'buy kake',
+        start: new Date(),
+        end: addHours( new Date(), 2 ),
+        bgColor: '#fafafa',
+        user: {
+            _id: 'asd123',
+            name: 'Jhon Doe'
+        }
+    }]
+
     return (
         <>
             <NavBar />
+
+            <Calendar
+                localizer={ localizer }
+                events={ events }
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+            />
         </>
     )
 }
