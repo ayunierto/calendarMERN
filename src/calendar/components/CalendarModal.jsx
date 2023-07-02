7e996e8e3a463c5d47d62b423d6dddd1e3db0c4a
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2023-07-02 06:44:42 -0500

    contenido del modal y datepicker

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -2,6 +2,11 @@ import Modal from 'react-modal'
 
 import './CalendarModal.css'
 import { useState } from 'react';
+import { addHours } from 'date-fns';
+
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const customStyles = {
     content: {
@@ -21,6 +26,26 @@ export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState( true )
 
+    const [formValues, setFormValues] = useState({
+        title: "John",
+        notes: "Doe",
+        start: new Date(),
+        end: addHours( new Date(), 2 )
+    })
+
+    const onInputChange = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [ target.name ]: target.value
+        })
+    }
+    const onDateChanged = ( date, changing) => {
+        setFormValues({
+            ...formValues,
+            [ changing ]: date
+        })
+    }
+
     const onCloseModal = () => {
         // console.log('cerrando modal')
         setIsOpen( false )
@@ -36,9 +61,68 @@ export const CalendarModal = () => {
             overlayClassName="modal-fondo"
             closeTimeoutMS={ 200 }
         >
-            <h1>Hello</h1>
+            <h1> Nuevo evento </h1>
             <hr />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat iure, modi, vitae totam dolor, quam veniam id est quo architecto numquam soluta eius neque nisi repudiandae autem animi voluptatem illo.</p>
+            <form className="container">
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label><br />
+                    <DatePicker 
+                        selected={ formValues.start  } 
+                        onChange={ ( date ) => onDateChanged( date, 'start' ) } 
+                        dateFormat="Pp"
+                        className='form-control d-block w-100'
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label><br />
+                    <DatePicker 
+                        minDate={ formValues.start }
+                        selected={ formValues.end  } 
+                        onChange={ ( date ) => onDateChanged( date, 'end' ) } 
+                        dateFormat="Pp"
+                        className='form-control d-block w-100'
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={ formValues.title }
+                        onChange={ onInputChange }
+                    />
+                    <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea 
+                        type="text" 
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={ formValues.notes }
+                        onChange={ onInputChange }
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+
+            </form>
         </Modal>
     )
 }
